146f5ab7296ccbb1475871809d010336ecee5240
edgar <harove@gmail.com>
2019-12-04 00:54:08 -0300

    adding useState hooked user to store

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
-import React from 'react';
-import {useSelector} from 'react-redux';
-
+import React, {useState} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import './app.css'
+import { addUserActionCreator } from './store/user.actions';
 
 
 const App = () => {
+    const [name, setName] = useState('hola')
     const store = useSelector(store => store );
+    const dispatch = useDispatch();
+
+    const addVegeta = () => {
+       dispatch(addUserActionCreator({
+           id:null,
+           name: 'Vegeta',
+       })); 
+    }
+
+    const addUser = ()=>{
+        dispatch(addUserActionCreator({
+            id:null,
+            name
+        }))
+    }
+
+
+    const handleOnChange = ({target}) => {
+        const {value} = target 
+        setName(value)
+     }
+ 
     return(
-        <pre>
-            {JSON.stringify(store)}
-        </pre>        
+        <div className="app">
+            <input type = "text" value = {name} onChange = {handleOnChange}/>
+            <button onClick = {addUser}>
+                Add Vegeta
+            </button>
+            <pre>
+                {JSON.stringify(store)}
+            </pre>
+        </div>  
     )
 }
 
